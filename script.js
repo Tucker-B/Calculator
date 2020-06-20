7e7b32246b2875865c2b0f7f1b587d68c8b248e8
Tucker Barton <hisashiburix@gmail.com>
2020-06-20 13:36:08 -0400

    Add index.html, styling and scripting files, TODO list. Basic functionality e.g. 2 numbers and 1 operator

---
+++ script.js
@@ -0,0 +1,123 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a , b) {
+
+    a = parseInt(a);
+    b = parseInt(b);
+
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+
+        case '-':
+            return subtract(a, b);
+            break;
+        
+        case '*':
+            return multiply(a, b);
+            break;
+
+        case '/':
+            return divide(a, b);
+            break;
+
+        default:
+            return "Not a valid operator";
+            break;
+    }
+}
+
+function includesNumber(str) {
+    let strArray = str.slice(0);
+    if (typeof strArray[0] == Number) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function containsOperator(operator) {
+    console.log("Inside containsOperator function");
+    switch (operator) {
+        case '+':
+            console.log("Returned true: +");
+            return true;
+            break;
+        case '-':
+            console.log("Returned true: -");
+            return true;
+            break;
+
+        case '*':
+            console.log("Returned true: *");
+            return true;
+            break;
+
+        case '/':
+            console.log("Returned true: /");
+            return true;
+            break;
+
+        default:
+            console.log("Returned false");
+            return false;
+            break;
+    }
+}
+
+var place = 0;
+var operator = '';
+var a = "";
+var b = "";
+
+document.getElementById("button-layout").addEventListener('click', (event) => {
+    
+    let labelElement = document.getElementById("num");
+    let labelContent = labelElement.textContent;
+    // Checks if input is in fact a number & !NaN
+    let parsedNumber = Number(event.target.id);
+    if (parsedNumber == event.target.id && place <= 2) {
+        labelElement.textContent += event.target.id;    
+        if (place < 2) {
+            place = 1;
+            a += event.target.id;
+        } else if (place == 2) {
+            b += event.target.id;
+        }
+    } else if (containsOperator(event.target.id) && place == 1) {
+        console.log("Inside second if statement");
+        labelElement.textContent += event.target.id;
+        place = 2;
+        operator = event.target.id;
+    } else if (place == 2 && event.target.id == '=') {
+        labelElement.textContent += event.target.id;
+        let numResult = operate(operator, a, b);
+        let labelResult = document.getElementById("result");
+        labelElement.textContent += numResult;
+        labelResult.textContent = numResult;
+        place = 3;
+    } else if (event.target.id == 'C') {
+        place = 0;
+        operator = '';
+        a = "";
+        b = "";
+        let labelResult = document.getElementById("result");
+        labelResult.textContent = "";
+        labelElement.textContent = "";
+    }
+});
