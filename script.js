f9f1981d4582fae8e793360fb5c3c1a163fc160a
Tucker Barton <hisashiburix@gmail.com>
2020-06-20 15:31:27 -0400

    Add keydown event functionality

--- script.js
+++ script.js
@@ -125,3 +125,115 @@ document.getElementById("button-layout").addEventListener('click', (event) => {
         labelElement.textContent = "";
     }
 });
+
+function determineKeycode(keyCode) {
+    switch (keyCode) {
+        case 48:
+            return '0';
+            break;
+        case 49:
+            return '1';
+            break;
+        case 50:
+            return '2';
+            break;
+        case 51:
+            return '3';
+            break;
+        case 52:
+            return '4';
+            break;
+        case 53:
+            return '5';
+            break;
+        case 54:
+            return '6';
+            break;
+        case 55:
+            return '7';
+            break;
+        case 56:
+            return '8';
+            break;
+        case 57:
+            return '9';
+            break;
+        case 187:
+            return '+';
+            break;
+        case 189:
+            return '-';
+            break;
+        case 170:
+            return '*';
+            break;
+        case 191:
+            return '/';
+            break;
+        case 190:
+            return '.';
+            break;
+        case 67: 
+            return 'C';
+            break;
+        default:
+            break;
+    }
+}
+
+window.addEventListener('keydown', (event) => {
+    let key = event.keyCode;
+    console.log(`key: ${key}`);
+    const dataKey = document.querySelector(`button[key="${key}"]`);
+    console.log(`dataKey: ${dataKey}, event.target.id: ${event.target.id}`);
+
+    if (dataKey) {
+        let labelElement = document.getElementById("num");
+        let labelContent = labelElement.textContent;
+        let userInput = determineKeycode(key);
+        // Checks if input is in fact a number & !NaN
+        let parsedNumber = Number(userInput);
+        // Also checks if next input contains a decimal
+        if (place <= 2 && (parsedNumber == userInput || userInput == '.')) {
+            labelElement.textContent += userInput;    
+            if (place < 2) {
+                place = 1;
+                a += userInput;
+            } else if (place == 2) {
+                b += userInput;
+            }
+        // Checks if input is an operator and if the equation only has 1 number, a
+        } else if (place == 1 && containsOperator(userInput)) {
+            labelElement.textContent += userInput;
+            place = 2;
+            operator = userInput;
+        // Checks if equation has 2 numbers & an operator, and if user clicked '=' or if dividing by 0
+        } else if (place == 2 && (userInput == '=' || (operator == '/' && b == '0'))) {
+            labelElement.textContent += '=';
+            let numResult = operate(operator, a, b);
+            let labelResult = document.getElementById("result");
+            labelElement.textContent += numResult;
+            labelResult.textContent = numResult;
+            place = 3;
+        // Allows additional operator(s) and therefore longer equations
+        // Checks if equation already has 2 numbers, an operator, and if input is another operator
+        } else if (place == 2 && containsOperator(userInput) && b != "") {
+            a = operate(operator, a, b);
+            b = "";
+            place = 2;
+            operator = userInput;
+            labelElement.textContent = a + operator;
+        // If user clicks 'C' Button, clear equation and labels
+        } else if (userInput == 'C') {
+            place = 0;
+            operator = '';
+            a = "";
+            b = "";
+            let labelResult = document.getElementById("result");
+            labelResult.textContent = "";
+            labelElement.textContent = "";
+
+
+        }
+    }
+});
