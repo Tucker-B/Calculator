d5850bd47de5580dee82a64fbe278d036c5f4c66
Tucker Barton <hisashiburix@gmail.com>
2020-06-20 15:40:35 -0400

    Clean up redundant code by moving it into the function calculator

--- script.js
+++ script.js
@@ -69,6 +69,51 @@ function containsOperator(operator) {
             break;
     }
 }
+
+function calculater(userInput, labelElement, labelContent) {
+    // Checks if input is in fact a number & !NaN
+    let parsedNumber = Number(userInput);
+    // Also checks if next input contains a decimal
+    if (place <= 2 && (parsedNumber == userInput || userInput == '.')) {
+        labelElement.textContent += userInput;    
+        if (place < 2) {
+            place = 1;
+            a += userInput;
+        } else if (place == 2) {
+            b += userInput;
+        }
+    // Checks if input is an operator and if the equation only has 1 number, a
+    } else if (place == 1 && containsOperator(userInput)) {
+        labelElement.textContent += userInput;
+        place = 2;
+        operator = userInput;
+    // Checks if equation has 2 numbers & an operator, and if user clicked '=' or if dividing by 0
+    } else if (place == 2 && (userInput == '=' || (operator == '/' && b == '0'))) {
+        labelElement.textContent += '=';
+        let numResult = operate(operator, a, b);
+        let labelResult = document.getElementById("result");
+        labelElement.textContent += numResult;
+        labelResult.textContent = numResult;
+        place = 3;
+    // Allows additional operator(s) and therefore longer equations
+    // Checks if equation already has 2 numbers, an operator, and if input is another operator
+    } else if (place == 2 && containsOperator(userInput) && b != "") {
+        a = operate(operator, a, b);
+        b = "";
+        place = 2;
+        operator = userInput;
+        labelElement.textContent = a + operator;
+    // If user clicks 'C' Button, clear equation and labels
+    } else if (userInput == 'C') {
+        place = 0;
+        operator = '';
+        a = "";
+        b = "";
+        let labelResult = document.getElementById("result");
+        labelResult.textContent = "";
+        labelElement.textContent = "";
+    }
+}
 // Keeps track of where in the equation we are. E.g. first num = 1, operator = 2, second num then '=' = 3
 var place = 0;
 // Operator for equation: '+', '-', '*', or '/'
@@ -82,48 +127,9 @@ document.getElementById("button-layout").addEventListener('click', (event) => {
     
     let labelElement = document.getElementById("num");
     let labelContent = labelElement.textContent;
-    // Checks if input is in fact a number & !NaN
-    let parsedNumber = Number(event.target.id);
-    // Also checks if next input contains a decimal
-    if (place <= 2 && (parsedNumber == event.target.id || event.target.id == '.')) {
-        labelElement.textContent += event.target.id;    
-        if (place < 2) {
-            place = 1;
-            a += event.target.id;
-        } else if (place == 2) {
-            b += event.target.id;
-        }
-    // Checks if input is an operator and if the equation only has 1 number, a
-    } else if (place == 1 && containsOperator(event.target.id)) {
-        labelElement.textContent += event.target.id;
-        place = 2;
-        operator = event.target.id;
-    // Checks if equation has 2 numbers & an operator, and if user clicked '=' or if dividing by 0
-    } else if (place == 2 && (event.target.id == '=' || (operator == '/' && b == '0'))) {
-        labelElement.textContent += '=';
-        let numResult = operate(operator, a, b);
-        let labelResult = document.getElementById("result");
-        labelElement.textContent += numResult;
-        labelResult.textContent = numResult;
-        place = 3;
-    // Allows additional operator(s) and therefore longer equations
-    // Checks if equation already has 2 numbers, an operator, and if input is another operator
-    } else if (place == 2 && containsOperator(event.target.id) && b != "") {
-        a = operate(operator, a, b);
-        b = "";
-        place = 2;
-        operator = event.target.id;
-        labelElement.textContent = a + operator;
-    // If user clicks 'C' Button, clear equation and labels
-    } else if (event.target.id == 'C') {
-        place = 0;
-        operator = '';
-        a = "";
-        b = "";
-        let labelResult = document.getElementById("result");
-        labelResult.textContent = "";
-        labelElement.textContent = "";
-    }
+
+    calculater(event.target.id, labelElement, labelContent);
+    
 });
 
 function determineKeycode(keyCode) {
@@ -185,55 +191,14 @@ window.addEventListener('keydown', (event) => {
     let key = event.keyCode;
     console.log(`key: ${key}`);
     const dataKey = document.querySelector(`button[key="${key}"]`);
-    console.log(`dataKey: ${dataKey}, event.target.id: ${event.target.id}`);
+
 
     if (dataKey) {
         let labelElement = document.getElementById("num");
         let labelContent = labelElement.textContent;
         let userInput = determineKeycode(key);
-        // Checks if input is in fact a number & !NaN
-        let parsedNumber = Number(userInput);
-        // Also checks if next input contains a decimal
-        if (place <= 2 && (parsedNumber == userInput || userInput == '.')) {
-            labelElement.textContent += userInput;    
-            if (place < 2) {
-                place = 1;
-                a += userInput;
-            } else if (place == 2) {
-                b += userInput;
-            }
-        // Checks if input is an operator and if the equation only has 1 number, a
-        } else if (place == 1 && containsOperator(userInput)) {
-            labelElement.textContent += userInput;
-            place = 2;
-            operator = userInput;
-        // Checks if equation has 2 numbers & an operator, and if user clicked '=' or if dividing by 0
-        } else if (place == 2 && (userInput == '=' || (operator == '/' && b == '0'))) {
-            labelElement.textContent += '=';
-            let numResult = operate(operator, a, b);
-            let labelResult = document.getElementById("result");
-            labelElement.textContent += numResult;
-            labelResult.textContent = numResult;
-            place = 3;
-        // Allows additional operator(s) and therefore longer equations
-        // Checks if equation already has 2 numbers, an operator, and if input is another operator
-        } else if (place == 2 && containsOperator(userInput) && b != "") {
-            a = operate(operator, a, b);
-            b = "";
-            place = 2;
-            operator = userInput;
-            labelElement.textContent = a + operator;
-        // If user clicks 'C' Button, clear equation and labels
-        } else if (userInput == 'C') {
-            place = 0;
-            operator = '';
-            a = "";
-            b = "";
-            let labelResult = document.getElementById("result");
-            labelResult.textContent = "";
-            labelElement.textContent = "";
 
-
-        }
+        calculater(userInput, labelElement, labelContent);
+        
     }
 });
